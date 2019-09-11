f3f0ceae3a0afef3082607d829a71bf3da0f6dc7
Kelvin Kaburu <kelvinmwenda9@gmail.com>
2019-09-11 14:36:40 +0300

    files added from figma

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Stage 2 Task | ohmydevOps Page</title>
+
+	<!-- added external css -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="cover">
+		<!-- ALL HTML CODE FROM FIGMA IN HERE -->
+
+		<?php 
+		
+			// echo your time function here
+
+		 ?>
+	</div>
+
+</body>
+</html>
